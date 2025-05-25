dbb44602f5a5072539353a935dbb5cb0cd0b474a
vmxmy <blueyang@gmail.com>
2025-05-25 23:23:28 +0800

    fix

--- webapp/core/config.py
+++ webapp/core/config.py
@@ -51,6 +51,7 @@ class Settings(BaseSettings):
                 "http://localhost:3000",
                 "http://localhost:5173",
                 "http://127.0.0.1:5173",
+                "http://10.10.10.16:5173",
                 "http://localhost:8080",
                 "http://127.0.0.1:8080",
                 "http://172.28.97.217:5173",
