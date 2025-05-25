f0296724dbe29f82d4f5de4c980296136723f166
vmxmy <blueyang@gmail.com>
2025-05-25 22:19:28 +0800

    dev remote open

--- frontend/v2/vite.config.ts
+++ frontend/v2/vite.config.ts
@@ -7,6 +7,7 @@ import path from 'path'; // 导入 path 模块
 export default defineConfig({
   plugins: [react()],
   server: {
+    host: '0.0.0.0',
     proxy: {
       '/api': {
         target: process.env.VITE_API_BASE_URL || 'http://localhost:8080', // 从环境变量读取后端服务地址，如果未设置则使用默认值
